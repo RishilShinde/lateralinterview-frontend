514ba984a48b73973ea0b5d376439f598ee5da74
Rishil Shinde <71715306+RishilShinde@users.noreply.github.com>
2026-01-30 17:25:04 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import your pages
 import Home from "./pages/Home";
 import DomainPage from "./pages/DomainPage";
 import ProviderPage from "./pages/ProviderPage";
+import SubCategoryPage from "./pages/SubCategoryPage";
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/domain/:id" element={<DomainPage />} />
         <Route path="/domain/:id/:providerId" element={<ProviderPage />} />
+        <Route
+          path="/domain/:id/:providerId/:subCategory"
+          element={<SubCategoryPage />}
+        />
       </Routes>
     </Router>
   );
