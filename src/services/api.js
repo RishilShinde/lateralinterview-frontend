fee1da9649c86596059bab7d846c5e138e615391
Rishil Shinde <71715306+RishilShinde@users.noreply.github.com>
2026-01-31 22:29:14 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 // const API_BASE = "http://127.0.0.1:7071/api";
 
-const API_BASE = "https://lateralinterview-backend-edbghsewhghgbvay.centralindia-01.azurewebsites.net/api";
+// const API_BASE = "https://lateralinterview-backend-edbghsewhghgbvay.centralindia-01.azurewebsites.net/api/getQuestions";
                              
 export const getQuestions = async () => {
-  const response = await axios.get(`${API_BASE}/getQuestions`);
+  const response = await axios.get("https://lateralinterview-backend-edbghsewhghgbvay.centralindia-01.azurewebsites.net/api/getQuestions");
   return response.data;
 };
+
 
 
